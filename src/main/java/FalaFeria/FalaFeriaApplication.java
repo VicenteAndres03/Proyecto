3c4add08bcc9c1676950fde3ca0cc70188ef544c
Vicente Pacheco <vicepachecoa@gmail.com>
2025-09-06 18:28:04 -0400

    FalaFeria

---
+++ src/main/java/FalaFeria/FalaFeriaApplication.java
@@ -0,0 +1,13 @@
+package FalaFeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaFeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaFeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FalaFeria/FalaFeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package FalaFeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalaFeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
