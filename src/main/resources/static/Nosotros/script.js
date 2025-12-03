f9f626aad10c8000c249247cbe1c482fbd54b0ca
Diego <di.echeverriaj@duocuc.cl>
2025-12-03 17:41:28 -0300

    Cree las carpentas frontend y backend, despúes movi cada archivo a su lado correspondiente y borre el archivo static ya que no es necesario ahora

--- src/main/resources/static/Nosotros/script.js
+++
@@ -1,7 +0,0 @@
-const hamburguesa = document.getElementById('MenuHamburguesa')
-const Menu = document.querySelector('.MenuNavegacion')
-
-//esto llama a la funcion show para que se muestren por pantalla
-hamburguesa.addEventListener('click', () => {
-    Menu.classList.toggle('show')
-})
